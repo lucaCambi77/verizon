b89e40c3511ca9943c032179202a4d9ae3f438b8
lucacambi@rentalcars.com <lucacambi@rentalcars.com>
2020-04-08 16:03:02 +0200

    code clean

--- src/main/java/it/cambi/verizon/service/AttendeeService.java
+++ src/main/java/it/cambi/verizon/service/AttendeeService.java
@@ -8,9 +8,6 @@ import it.cambi.verizon.mongo.repository.AttendeeRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -23,7 +20,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @Slf4j
 public class AttendeeService {
-    
+
     private final AttendeeRepository attendeeRepository;
 
     public List<Attendee> findAll() {
